acdeb6a956f57d42eeedf158b369d0734864b4e2
Hevin Jant <hevin.jant@gmail.com>
2022-05-03 00:39:50 -0700

    first commit to repository

---
+++ dataProcessing.py
@@ -0,0 +1,90 @@
+import os
+import pandas as pandas
+import numpy as numpy
+import seaborn as sns
+import matplotlib.pyplot as plt
+import librosa
+import librosa.display
+from IPython.display import Audio
+import warnings
+warnings.filterwarnings('ignore')
+
+def printAllSoundFiles(path):
+    for dirname, _, filenames in os.walk(folderPath):
+        for filename in filenames:
+            print(os.path.join(dirname, filename))
+
+def loadDatasets(path):
+    """ 
+        Label the audio file to its associated emotion. 
+        paths[] stores all the audio file's path, labels[] stores the label of the audio file with the same index.
+    """
+    paths = []
+    labels = []
+    for dirname, _, filenames in os.walk(path):
+        for filename in filenames:
+            paths.append(os.path.join(dirname, filename))
+            label = filename.split('_')[-1]
+            label = label.split('.')[0]
+            labels.append(label.lower())
+
+    paths.pop(0)
+    labels.pop(0)
+
+    return paths, labels
+
+def createDataframe(paths, labels):
+    dataframe = pandas.DataFrame()
+    dataframe['speech'] = paths
+    dataframe['label'] = labels
+    
+    # print(dataframe.head())
+    # print(dataframe['label'].value_counts())
+
+    return dataframe
+
+def showDataCountGraph(data):
+    sns.countplot(data)
+    plt.xlabel('Emotions')
+    plt.ylabel('Data counts')
+    plt.show()
+
+def showWaveplot(data, sampleRate, emotion):
+    plt.figure(figsize=(10, 4))
+    plt.title(emotion, size=20)
+    librosa.display.waveshow(data, sr=sampleRate)
+    plt.show()
+
+def showSpectogram(data, sampleRate, emotion):
+    x = librosa.stft(data)
+    xdb = librosa.amplitude_to_db(abs(x))
+
+    plt.figure(figsize=(10, 4))
+    plt.title(emotion, size=20)
+    librosa.display.specshow(xdb, sr=sampleRate, x_axis='time', y_axis='hz')
+    plt.colorbar()
+    plt.show()
+
+def showWaveplotAndSpectogramForEmotion(dataframe, emotion):
+    path = numpy.array(dataframe['speech'][dataframe['label']==emotion])[0]
+    data, sampleRate = librosa.load(path)
+    showWaveplot(data, sampleRate, emotion)
+    showSpectogram(data, sampleRate, emotion)
+
+def extractMFCC(filename):
+    data, sampleRate = librosa.load(filename, duration=3, offset=0.5)
+    mfcc = numpy.mean(librosa.feature.mfcc(y=data, sr=sampleRate, n_mfcc=40).T, axis=0)
+    return mfcc
+
+def extractMFCCfromAllFiles(dataframe):
+    x_mfcc = dataframe['speech'].apply(lambda x: extractMFCC(x))
+    return x_mfcc
+
+def main():
+    #printAllSoundFiles('./datasets')
+    paths, labels = loadDatasets('./datasets')
+    dataframe = createDataframe(paths, labels)
+    #showDataCountGraph(dataframe['label'])
+    #showWaveplotAndSpectogramForEmotion(dataframe, 'angry')
+
+main()
